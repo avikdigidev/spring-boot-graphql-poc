b7438633d49c1bc0ff87d5af4dcf48692bda55d3
avikdigidev <avikdigidev@gmail.com>
2022-10-16 16:45:56 +0530

    Initial commit with dummy query

---
+++ src/main/java/com/avikdigidev/springboot/graphql/GraphQLWithSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.avikdigidev.springboot.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class GraphQLWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQLWithSpringBootApplication.class, args);
+	}
+
+}
