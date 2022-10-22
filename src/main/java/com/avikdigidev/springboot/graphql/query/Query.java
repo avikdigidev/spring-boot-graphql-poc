d645f087f87bd89b8afb1ff4b4d55534ff50734f
avikdigidev <avikdigidev@gmail.com>
2022-10-23 01:02:32 +0530

    Added Query with input parameters

--- src/main/java/com/avikdigidev/springboot/graphql/query/Query.java
+++ src/main/java/com/avikdigidev/springboot/graphql/query/Query.java
@@ -7,11 +7,16 @@ import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 @Component
 public class Query implements GraphQLQueryResolver {
 
-	public String firstQuery () {
-		return "First Query";
-	}
-	
-	public String secondQuery () {
-		return "Second Query";
-	}
+    public String firstQuery() {
+
+        return "First Query";
+    }
+
+    public String secondQuery() {
+
+        return "Second Query";
+    }
+    public String fullName(String firstName, String lastName){
+        return firstName + " " + lastName;
+    }
 }
