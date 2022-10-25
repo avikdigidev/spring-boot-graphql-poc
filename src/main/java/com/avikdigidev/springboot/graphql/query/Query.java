8a0316b008d7dfab6ba1236b8200cddd0e7a70b2
avikdigidev <avikdigidev@gmail.com>
2022-10-25 13:29:08 +0530

    Added GraphQL Query with Response Model Class

--- src/main/java/com/avikdigidev/springboot/graphql/query/Query.java
+++ src/main/java/com/avikdigidev/springboot/graphql/query/Query.java
@@ -1,12 +1,16 @@
 package com.avikdigidev.springboot.graphql.query;
 
 import com.avikdigidev.springboot.graphql.request.NameRequest;
+import com.avikdigidev.springboot.graphql.response.StudentResponse;
+import com.avikdigidev.springboot.graphql.service.StudentService;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Query implements GraphQLQueryResolver {
-
+@Autowired
+    StudentService studentService;
     public String firstQuery() {
 
         return "First Query";
@@ -16,11 +20,16 @@ public class Query implements GraphQLQueryResolver {
 
         return "Second Query";
     }
-    public String fullName(String firstName, String lastName){
+
+    public String fullName(String firstName, String lastName) {
         return firstName + " " + lastName;
     }
 
-    public String fullNameRequest(NameRequest name){
-        return name.getFirstName() + " "+name.getLastName();
+    public String fullNameRequest(NameRequest name) {
+        return name.getFirstName() + " " + name.getLastName();
+    }
+
+    public StudentResponse getStudent(long id){
+        return new StudentResponse(studentService.getStudentById(id));
     }
 }
